fd62ee4ee40a09ccd5373df2afbe8e726eb5f14e
Daurenov <3350698@mail.ru>
2022-03-17 07:41:32 +0600

    заполнение таблиц core и получение данных fetchall() без листа

--- connection.py
+++ connection.py
@@ -114,97 +114,9 @@ order_lines = Table('order_lines', metadata,
                         )
 
 
-items_list = [
-    {
-        "name":"Chair",
-        "cost_price": 9.21,
-        "selling_price": 10.81,
-        "quantity": 6
-    },
-    {
-        "name":"Pen",
-        "cost_price": 3.45,
-        "selling_price": 4.51,
-        "quantity": 3
-    },
-    {
-        "name":"Headphone",
-        "cost_price": 15.52,
-        "selling_price": 16.81,
-        "quantity": 50
-    },
-    {
-        "name":"Travel Bag",
-        "cost_price": 20.1,
-        "selling_price": 24.21,
-        "quantity": 50
-    },
-    {
-        "name":"Keyboard",
-        "cost_price": 20.12,
-        "selling_price": 22.11,
-        "quantity": 50
-    },
-    {
-        "name":"Monitor",
-        "cost_price": 200.14,
-        "selling_price": 212.89,
-        "quantity": 50
-    },
-    {
-        "name":"Watch",
-        "cost_price": 100.58,
-        "selling_price": 104.41,
-        "quantity": 50
-    },
-    {
-        "name":"Water Bottle",
-        "cost_price": 20.89,
-        "selling_price": 25.00,
-        "quantity": 50
-    },
-]
-
-order_list = [
-    {
-        "customer_id": 1
-    },
-    {
-        "customer_id": 1
-    }
-]
-
-order_line_list = [
-    {
-        "order_id": 1,
-        "item_id": 1,
-        "quantity": 5
-    },
-    {
-        "order_id": 1,
-        "item_id": 2,
-        "quantity": 2
-    },
-    {
-        "order_id": 1,
-        "item_id": 3,
-        "quantity": 1
-    },
-    {
-        "order_id": 2,
-        "item_id": 1,
-        "quantity": 5
-    },
-    {
-        "order_id": 2,
-        "item_id": 2,
-        "quantity": 5
-    },
-]
-
-
-
 conn = connections("aclchemyorder")
+
+## добавление записей в табицы по списку
 # conn.execute(insert(items), items_list)
 # conn.execute(insert(orders), order_list)
 # conn.execute(insert(order_lines), order_line_list)
